fada9f1d3073def14c0bc259e00796b3064a9e51
Daniil <daniilminin1990@yandex.ru>
2024-01-10 02:19:39 +0600

    localStorage, useEffect на сохранение и функ на возврат при обновлении

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,32 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useEffect, useState } from 'react';
 import './App.css';
 
 function App() {
+
+  const initVal = () => {
+    let valueAsString = localStorage.getItem('counterValue')
+    if (valueAsString) {
+      return JSON.parse(valueAsString)
+    } else {
+      return 0
+    }
+  }
+
+  const [value, setValue] = useState(initVal)
+
+  useEffect(() => {
+    localStorage.setItem('counterValue', JSON.stringify(value))
+  }, [value])
+
+  const incHandler = () => {
+    setValue(value + 1)
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h3>{value}</h3>
+      <button onClick={incHandler}>increase</button>
+
     </div>
   );
 }
